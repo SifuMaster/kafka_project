e0d6d6224ed6d55c86eb8b8798c6d85da0efa450
sifumaster <phivos93@yahoo.com>
2023-06-22 15:04:27 +0300

    Update script

--- preparation.sh
+++ preparation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Create topics
 sudo su - kafka -c "~/kafka/bin/kafka-topics.sh --create --topic for_mappers_1 --bootstrap-server localhost:9092"
 
 sudo su - kafka -c "~/kafka/bin/kafka-topics.sh --create --topic for_mappers_2 --bootstrap-server localhost:9092"
@@ -7,3 +8,8 @@ sudo su - kafka -c "~/kafka/bin/kafka-topics.sh --create --topic for_mappers_2 -
 sudo su - kafka -c "~/kafka/bin/kafka-topics.sh --create --topic for_reducers_1 --bootstrap-server localhost:9092"
 
 sudo su - kafka -c "~/kafka/bin/kafka-topics.sh --create --topic for_reducers_2 --bootstrap-server localhost:9092"
+
+# Exctract dataset
+mkdir ./dataset
+unzip dataset.zip -d dataset/
+mv ./dataset/2013-11\ -\ Citi\ Bike\ trip\ data.csv ./dataset/Citi_Bike_trip_data.csv
